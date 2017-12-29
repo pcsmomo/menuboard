e01310c729c5919b1dd797a29e7d0c452f02e54d
NoahKim <dongwoo_333@hanmail.net>
2017-12-29 17:52:07 +1100

    61. Installing Root Modules

---
+++ client/src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/App.css";
+
+const App = () => {
+  return <div>Hi, there!</div>;
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
 
-ReactDOM.render();
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
