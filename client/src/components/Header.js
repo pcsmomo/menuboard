604ffba1ddbf35be583e7bb9cacd011451fb32de
NoahKim <dongwoo_333@hanmail.net>
2018-01-05 17:46:56 +1100

    align contents center change the position of logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 class Header extends Component {
   renderHeader() {
     return (
-      <div>
+      <header>
         <div className="title">
           <div className="mainTitle">FROM THE KITCHEN</div>
           <div className="subTitle">BREAKFAST & LUNCH ALL DAY</div>
         </div>
         <img className="logo" src="/images/jethro_logo.png" alt="logo" />
-      </div>
+      </header>
     );
   }
 
