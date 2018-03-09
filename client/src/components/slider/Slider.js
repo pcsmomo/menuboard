64bff64fb62cc94e5b4bd75f481246c58350254d
NoahKim <dongwoo_333@hanmail.net>
2018-03-09 14:20:01 +1100

    adjust media query for wide screen

--- client/src/components/slider/Slider.js
+++ client/src/components/slider/Slider.js
@@ -26,42 +26,44 @@ class Slider extends Component {
       <div className="sliderArea" style={this.props.toggle ? display : hide}>
         <div className="slider">
           <h2>{this.props.curCateName}</h2>
-          <div className="areaImg">
-            <div
-              className="btnClose"
-              onClick={() => this.props.closeSlider()}
-            />
-            <img
-              className="mainImg"
-              src={"images/" + this.props.curItem.id + ".jpg"}
-              alt={this.props.curItem.name}
-            />
-            <label
-              className="prev"
-              onClick={() =>
-                this.props.fetchDishInfo(this.props.curItem.id, "prev")
-              }
-            >
-              <span />
-            </label>
-            <label
-              className="next"
-              onClick={() =>
-                this.props.fetchDishInfo(this.props.curItem.id, "next")
-              }
-            >
-              <span />
-            </label>
-          </div>
-          <div className="sliderDetail">
-            <h4>{this.props.curItem.name}</h4>
-            <p>
-              {this.props.curItem.desc} <span>{this.props.curItem.type}</span>
-            </p>
-            <p>{this.props.curItem.price}</p>
-            {this.props.curItem.add
-              ? this.renderAdd(this.props.curItem.add)
-              : ""}
+          <div className="sliderDisplay">
+            <div className="areaImg">
+              <div
+                className="btnClose"
+                onClick={() => this.props.closeSlider()}
+              />
+              <img
+                className="mainImg"
+                src={"images/" + this.props.curItem.id + ".jpg"}
+                alt={this.props.curItem.name}
+              />
+              <label
+                className="prev"
+                onClick={() =>
+                  this.props.fetchDishInfo(this.props.curItem.id, "prev")
+                }
+              >
+                <span />
+              </label>
+              <label
+                className="next"
+                onClick={() =>
+                  this.props.fetchDishInfo(this.props.curItem.id, "next")
+                }
+              >
+                <span />
+              </label>
+            </div>
+            <div className="sliderDetail">
+              <h4>{this.props.curItem.name}</h4>
+              <p>
+                {this.props.curItem.desc} <span>{this.props.curItem.type}</span>
+              </p>
+              <p>{this.props.curItem.price}</p>
+              {this.props.curItem.add
+                ? this.renderAdd(this.props.curItem.add)
+                : ""}
+            </div>
           </div>
         </div>
       </div>
